32031489f387402116087c4a248f50427e8e7a54
Youssef Bat <youssef.bat@students.salle.url.edu>
2022-04-20 12:42:53 +0200

    events screen for maps initialized

--- app/src/main/java/edu/url/salle/albert/gt/evased/events_screen_activity.java
+++ app/src/main/java/edu/url/salle/albert/gt/evased/events_screen_activity.java
@@ -1,16 +1,13 @@
 package edu.url.salle.albert.gt.evased;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class events_screen_activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_events_screen);
     }
 }
