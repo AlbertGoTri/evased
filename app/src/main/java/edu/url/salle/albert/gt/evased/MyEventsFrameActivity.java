19aa492cf883c70bf66e5c818e431873e69e254f
Robert Elies <robert.elies@students.salle.url.edu>
2022-04-20 18:50:44 +0200

    marcel + robert commit for 20/4/2022

---
+++ app/src/main/java/edu/url/salle/albert/gt/evased/MyEventsFrameActivity.java
@@ -0,0 +1,14 @@
+package edu.url.salle.albert.gt.evased;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyEventsFrameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_events_frame);
+    }
+}
---
+++ app/src/main/java/edu/url/salle/albert/gt/evased/MyeventsframeSingleActivity.java
@@ -0,0 +1,14 @@
+package edu.url.salle.albert.gt.evased;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyeventsframeSingleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myeventsframe_single);
+    }
+}
