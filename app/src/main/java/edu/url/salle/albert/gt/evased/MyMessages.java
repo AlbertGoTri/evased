dc487bdd2afc881ba908bd0ff899622104541584
Youssef Bat <youssef.bat@students.salle.url.edu>
2022-05-17 22:06:08 +0200

    merged with dev

--- app/src/main/java/edu/url/salle/albert/gt/evased/MyMessages.java
+++ app/src/main/java/edu/url/salle/albert/gt/evased/MyMessages.java
@@ -10,5 +10,6 @@ public class MyMessages extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_messages);
+        //
     }
 }
