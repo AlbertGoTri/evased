4e89fe78071354c9817cbca1f233c3de7b4e3ba2
Marcel Farrés Del-Caño <marcel.farres@students.salle.url.edu>
2022-06-12 23:56:58 +0200

    trying things3

--- app/src/main/java/edu/url/salle/albert/gt/evased/MyMessages.java
+++ app/src/main/java/edu/url/salle/albert/gt/evased/MyMessages.java
@@ -14,11 +14,22 @@ import java.util.ConcurrentModificationException;
 
 import edu.url.salle.albert.gt.evased.Adapters.MyRecyclerViewMessagesAdapter;
 import edu.url.salle.albert.gt.evased.entities.Conversation;
+import edu.url.salle.albert.gt.evased.entities.Event;
 import edu.url.salle.albert.gt.evased.entities.User;
+import edu.url.salle.albert.gt.evased.lab.EventLab;
+import edu.url.salle.albert.gt.evased.lab.UserLab;
 
 
 public class MyMessages extends AppCompatActivity implements MyRecyclerViewMessagesAdapter.ItemClickListener {
 
+    //---------------------------------------------------------------------------------INITIALIZE USERS + CONVERSATIONS
+    UserLab userlab = new UserLab();
+    ArrayList<User> users = userlab.getUsers();
+    ArrayList<Conversation> conversations = userlab.getConversations();
+
+    //---------------------------------------------------------------------------------INITIALIZE EVENTS
+    EventLab eventlab = new EventLab( users);
+    ArrayList<Event> events = eventlab.getEvents();
 
     private MyRecyclerViewMessagesAdapter mAdapter;
 
@@ -30,15 +41,15 @@ public class MyMessages extends AppCompatActivity implements MyRecyclerViewMessa
         //intent get information
         Intent intent = this.getIntent();
         Bundle bundle = intent.getExtras();
-        ArrayList<Conversation> conversations = (ArrayList<Conversation>) bundle.getSerializable("conversations");
-        User user = (User) getIntent().getSerializableExtra("user");
+        //ArrayList<Conversation> conversations = (ArrayList<Conversation>) bundle.getSerializable("conversations");
+        //User user = (User) getIntent().getSerializableExtra("user");
 
         //arraylist with the converations with the user participates
         ArrayList<Conversation> final_conversations = new ArrayList<>();
 
         //get only the conversations where user participates
         for(Conversation conv: conversations){
-            if(conv.getReceiver() == user || conv.getSender() == user ){
+            if(conv.getReceiver() == users.get(0) || conv.getSender() == users.get(0) ){
                 final_conversations.add(conv);
 
             }
