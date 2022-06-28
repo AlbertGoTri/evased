479f9292ab5e202bfa81abe57ac3da98ea420100
Marcel Farrés Del-Caño <marcel.farres@students.salle.url.edu>
2022-06-28 07:18:00 +0200

    API

--- app/src/main/java/edu/url/salle/albert/gt/evased/MainActivity.java
+++ app/src/main/java/edu/url/salle/albert/gt/evased/MainActivity.java
@@ -2,12 +2,15 @@ package edu.url.salle.albert.gt.evased;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,6 +26,8 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Objects;
+
 import Persistence.MySingleton;
 import edu.url.salle.albert.gt.evased.Managers.UserConvEventManager;
 import edu.url.salle.albert.gt.evased.entities.User;
@@ -31,35 +36,27 @@ import edu.url.salle.albert.gt.evased.entities.User;
 public class MainActivity extends AppCompatActivity {
 
 
-    private Button noAccountBtn;
-    private Button signInBtn;
-    private TextView username;
-    private TextView password;
-    private String usernameValue;
-    private String passwordValue;
+    private EditText username;
+    private EditText password;
 
-    //---------------------------------------------------------------------------------EXTRA BUTTONS
-    private Button MyMessagesBTN;
-    private Button MyTimelineBTN;
+    private String userToken;
+
+    //check before continue to the next page
+    boolean error_detected;
 
     //----------------------------------------------------------------------------------CREATE ALL DATA (EVENTS, USERS, CONVERSATIONS)
     UserConvEventManager manager = new UserConvEventManager();
 
-    SharedPreferences preferences;
-
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        preferences = getSharedPreferences("Userinfo", 0);
-        username = findViewById(R.id.username);
-        password = findViewById(R.id.password);
+        //preferences = getSharedPreferences("Userinfo", 0);
+        //username = findViewById(R.id.username);
+        //password = findViewById(R.id.password);
 
-
-
-        noAccountBtn = findViewById(R.id.noAccountBtn);
+/*
+        Button noAccountBtn = findViewById(R.id.noAccountBtn);
         noAccountBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,41 +65,68 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        signInBtn = findViewById(R.id.signInBtn);
-        signInBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //check the user data
-                usernameValue = username.getText().toString();
-                passwordValue = password.getText().toString();
+ */
+        this.username = findViewById(R.id.username);
+        this.password = findViewById(R.id.password);
 
-                //TODO CAMBIAR
-                String url = "http://puigmal.salle.url.edu/api/v2/login";
+        //we create a shared preferences to be always able to access the user that signed in
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences.Editor editor = preferences.edit();
 
-                JSONObject jsonBody = new JSONObject();
-                try {
-                    jsonBody.put("email", "mf@gmail.com");
-                    jsonBody.put("password", "12345678");
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                        (url, jsonBody, response -> {
-                            try {
-                                String token = response.getString("accessToken");
-                                System.out.println(token);
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }, error -> {
-                            System.out.println("error");
 
-                        });
+        error_detected = false;
 
-                MySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjectRequest);
+        final Button signInBtn = findViewById(R.id.signInBtn);
+        signInBtn.setOnClickListener(v -> {
+
+            String email = this.username.getText().toString();
+            String password = this.password.getText().toString();
+            String url = "http://puigmal.salle.url.edu/api/v2/users";
+            JSONObject jsonBody = new JSONObject();
+            try {
+                jsonBody.put("email", email);
+                jsonBody.put("password", password);
+            } catch (JSONException e) {
+                e.printStackTrace();
+                error_detected = true;
+            }
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                    (url, jsonBody, response -> {
+                        try {
+                            String token = response.getString("accessToken");
+                            //setUser(token, email);
+                            System.out.println(token);
+                            this.userToken = token;
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            error_detected = true;
+                        }
+                    }, error -> {
+                        System.out.println("erroooooor");
+                        error_detected = true;
+                    });
+
+            MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+            Intent intent = new Intent(getApplicationContext(), Events_screen_activity.class);
+            if(!error_detected){
+                editor.putString("userToken",userToken);
+                editor.putString("email",email);
+                editor.putString("password",password);
+                editor.apply();
+
+
+            }else{
+                editor.putString("userToken","userToken");
+                editor.putString("email","jose@gmail.com");
+                editor.putString("password","password");
+                editor.apply();
 
             }
+
+            startActivity(intent);
+
         });
+
 
     }
 }
